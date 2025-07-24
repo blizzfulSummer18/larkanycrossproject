a562795a6d859e2a8934528f2441a49981e7e2f8
blizzfulSummer18 <efren.letran@hotelstotsenberg.com>
2025-07-24 11:41:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001; // 👈 now defaults to port 3001
+const PORT = process.env.PORT; //|| 3001;  👈 now defaults to port 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
